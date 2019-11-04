d5de49fe5e203f730c884cd41a44789d6154580e
David Lee <ufocrew@gmail.com>
2019-11-05 07:44:47 +0800

    update

--- examples/reflection_port_example.py
+++ examples/reflection_port_example.py
@@ -3,7 +3,7 @@ from resonator_tools import circuit
 
 
 port1 = circuit.reflection_port()
-port1.add_fromtxt('S11.txt','dBmagphasedeg',1)
+port1.add_fromtxt(r'examples/S11.txt','dBmagphasedeg',1)
 port1.autofit()
 print("Fit results:", port1.fitresults)
 port1.plotall()
